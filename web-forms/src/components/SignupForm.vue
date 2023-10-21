3d7866745bbc281706875253c97ebc83d3ff7f4c
mukhtarhan <mukhtarhanserikbay2003@gmail.com>
2023-10-21 20:08:12 +0600

    upd

--- web-forms/src/components/SignupForm.vue
+++ web-forms/src/components/SignupForm.vue
@@ -1,14 +1,58 @@
 <template>
   <form>
     <label>Email:</label>
-    <input type="email" required>
+    <input type="email" required v-model="email">
+
+    <label>Password:</label>
+    <input type="password" required v-model="password">
+
+    <label>Role:</label>
+    <select v-model="role">
+        <option value="developer">Web Developer</option>
+        <option value="designer">Web Designer</option>
+    </select>
+
+    <label>Skills:</label>
+    <input type="text" v-model="tempSkill" @keyup="addSkill">
+
+    <div v-for="skill in skills" :key="skill" class="pill">
+        {{ skill }}
+    </div>
+
+    <div class="terms">
+        <input type="checkbox" required v-model="terms">
+        <label>Accept terms and conditions</label>
+    </div>
+    
 
   </form>
+
+  <p>Email: {{ email }}</p>
+  <p>Password: {{ password }}</p>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return{
+            email: '',
+            password: '',
+            role: '',
+            terms: false,
+            tempSkill: '',
+            skills: []
+        }
+    },
+    methods: {
+        addSkill(e) {
+            if(e.key === ',' && this.tempSkill){
+                if(!this.skills.includes(this.tempSkill)){
+                    this.skills.push(this.tempSkill)
+                }
+                this.tempSkill = ''
+            }
+        }
+    }
 }
 </script>
 
@@ -30,7 +74,7 @@ export default {
         letter-spacing: 1px;
         font-weight: bold;
     }
-    input{
+    input, select{
         display: block;
         padding: 10px 6px;
         width: 100%;
@@ -38,5 +82,12 @@ export default {
         border: none;
         border-bottom: 1px solid #ddd;
         color: #555;
+    }
+    input[type="checkbox"]{
+        display: inline-block;
+        width: 16px;
+        margin: 0 10px 0 0;
+        position: relative;
+        top: 2px;
     }
 </style>
