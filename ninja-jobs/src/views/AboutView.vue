fa263caa9c79c975740755958689b6ec0aba6029
mukhtarhan <mukhtarhanserikbay2003@gmail.com>
2023-10-22 10:24:14 +0600

    upd

--- ninja-jobs/src/views/AboutView.vue
+++ ninja-jobs/src/views/AboutView.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui adipisci magni sunt inventore, officia quidem ipsa beatae odit maiores, debitis aliquid perferendis, iusto quae voluptas enim quasi repellat ab sit.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui adipisci magni sunt inventore, officia quidem ipsa beatae odit maiores, debitis aliquid perferendis, iusto quae voluptas enim quasi repellat ab sit.</p>
   </div>
 </template>
--- ninja-jobs/src/views/HomeView.vue
+++ ninja-jobs/src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui adipisci magni sunt inventore, officia quidem ipsa beatae odit maiores, debitis aliquid perferendis, iusto quae voluptas enim quasi repellat ab sit.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui adipisci magni sunt inventore, officia quidem ipsa beatae odit maiores, debitis aliquid perferendis, iusto quae voluptas enim quasi repellat ab sit.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
