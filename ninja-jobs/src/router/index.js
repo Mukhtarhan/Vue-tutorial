447c066313dd37ba5cc95f632d112aa5cf88d5f8
mukhtarhan <mukhtarhanserikbay2003@gmail.com>
2023-10-22 10:49:07 +0600

    upd

--- ninja-jobs/src/router/index.js
+++ ninja-jobs/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import Jobs from '../views/jobs/Jobs.vue' 
-
+import JobDetails from '../views/jobs/JobDetails.vue' 
 
 const routes = [
   {
@@ -19,6 +19,12 @@ const routes = [
     path: '/jobs',
     name: 'Jobs',
     component: Jobs
+  },
+  {
+    path: '/jobs/:id',
+    name: 'JobDetails',
+    component: JobDetails,
+    props: true
   }
 ]
 
